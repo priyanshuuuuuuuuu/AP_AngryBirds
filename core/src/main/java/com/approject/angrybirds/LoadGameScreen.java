d4a0836e1870ebf4fc77d4e228f5ca833cbe2da7
priyanshuuuuuuuuu <priyanshu23406@iiitd.ac.in>
2024-10-27 14:59:35 +0530

    final Static GUI

---
+++ core/src/main/java/com/approject/angrybirds/LoadGameScreen.java
@@ -0,0 +1,10 @@
+package com.approject.angrybirds;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+
+public class LoadGameScreen extends ScreenAdapter {
+    public static void main(String[] args) {
+        System.out.println("Not initialized till now.");
+    }
+}
